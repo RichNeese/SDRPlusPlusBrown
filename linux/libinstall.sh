4efd4b245c09d524af041c7e75bc7a36a80a5357
Kenneth BedFord <98486007+ksbjr@users.noreply.github.com>
2024-03-08 17:56:03 -0500

    fix libinstall.sh for linux installs

--- linux/libinstall.sh
+++ linux/libinstall.sh
@@ -10,19 +10,6 @@ echo "First we need to make sure dependancies are installed."
 echo "There may be several packages to install.  This could"
 
 echo "take several minutes to complete."
-#####################################################
-#
-# ENABLE SOAPYSDR DRIVERS
-# if you enable all the soapy drivers it will disable the
-# default libs/drivers that allow use with out SoapySDR
-#
-# Defualt for all is N
-#
-# Must be enable SOAPYSDR=Y to install SoapySDR and modules
-#
-######################################################
-
-SOAPYSDR=N 
 
 ################################################################
 #
@@ -30,82 +17,85 @@ SOAPYSDR=N
 #
 ################################################################
 echo "Installing Deps for building SDR++ / SDR++Brown"
-apt -y install build-essential
-apt -y install cmake
-apt -y install pkg-config
-apt -y install libcodec2-dev
-apt -y install libgtk-3-dev
-apt -y install portaudio19-dev
-apt -y install libfftw3-dev
-apt -y install libpulse-dev
-apt -y install libsoundio-dev
-apt -y install libasound2-dev
-apt -y install libusb-dev
-apt -y install libglfw3-dev
-apt -y install libiio-dev
-apt -y install libiio-utils
-apt -y install libad9361-dev
-apt -y install libvolk2-dev
-apt -y install libvulkan-volk-dev
-apt -y install libzstd-dev
-apt -y install zstd
+sudo apt -y install build-essential
+sudo apt -y install cmake
+sudo apt -y install pkg-config
+sudo apt -y install libcodec2-dev
+sudo apt -y install libgtk-3-dev
+sudo apt -y install portaudio19-dev
+sudo apt -y install libfftw3-dev
+sudo apt -y install libpulse-dev
+sudo apt -y install libsoundio-dev
+sudo apt -y install libasound2-dev
+sudo apt -y install libusb-dev
+sudo apt -y install libglfw3-dev
+sudo apt -y install libiio-dev
+sudo apt -y install libiio-utils
+sudo apt -y install libad9361-dev
+sudo apt -y install libvolk2-dev
+sudo apt -y install libvulkan-volk-dev
+sudo apt -y install libzstd-dev
+sudo apt -y install zstd
+echo ""
 
 #########################################################
 # Python make
 #########################################################
 echo "INSTALLING MAKO FOR PYTHON3"
-apt -y install python3-mako
+sudo apt -y install python3-mako
+echo ""
 
 #########################################################
 # Lib for bladerf
 #########################################################
 echo "INSTALLING BLADERF"
-apt -y install libbladerf-dev
-apt -y install bladerf-firmware-fx3
-apt -y install bladerf-fpga
-apt -y install bladerf-fpga-hostedx115
-apt -y install bladerf-fpga-hostedx40
-apt -y install bladerf-fpga-hostedxa4
-apt -y install bladerf-fpga-hostedxa5
-apt -y install bladerf-fpga-hostedxa9
+sudo apt -y install bladerf
+sudo apt -y install libbladerf-dev
+sudo apt -y install bladerf-firmware-fx3
+sudo apt -y install bladerf-fpga-hostedx115
+sudo apt -y install bladerf-fpga-hostedx40
+sudo apt -y install bladerf-fpga-hostedxa4
+sudo apt -y install bladerf-fpga-hostedxa5
+sudo apt -y install bladerf-fpga-hostedxa9
+echo ""
 
 #########################################################
 # Lib for RTL_SDR
 #########################################################
 echo "Installing RTL-SDR"
-apt -y install librtlsdr-dev
-apt -y install rtl-sdr
+sudo apt -y install librtlsdr-dev
+sudo apt -y install rtl-sdr
+echo ""
 
 #########################################################
 # Lib for airspy / airspyhf
 # Disable if you want to us soapysdr drivers
 #########################################################
-if [ SOAPYSDR == N ]; then
-	echo "INSTALLING AIRSPY"
-	apt -y install airspy
-	apt -y install libairspy-dev
-fi
+echo "INSTALLING AIRSPY"
+sudo apt -y install airspy
+sudo apt -y install libairspy-dev
 
-if [ SOAPYSDR == N ]; then
-	echo "Installing AIRSPYHF"
-	apt -y install airspyhf
-	apt -y install libairspyhf-dev
-fi
+echo "Installing AIRSPYHF"
+sudo apt -y install airspyhf
+sudo apt -y install libairspyhf-dev
+echo ""
 
 #########################################################
 # Lib for hackers
 # Disable if you want to us soapysdr drivers
 #########################################################
 echo "Installing HackRF"
-apt -y install hackrf
-apt -y install hackrf-dev
+sudo apt -y install hackrf
+sudo apt -y install libhackrf-dev
+echo ""
 
 #########################################################
 # Limes suite
 #########################################################
 echo "Installing LimeSuite"
-apt -y install limesuite
-apt -y install limesuite-dev
+sudo apt -y install limesuite
+sudo apt -y install liblimesuite-dev
+echo ""
 
 ################################################################
 #
@@ -114,49 +104,39 @@ apt -y install limesuite-dev
 # 
 #
 ################################################################
+echo "Installing SoapySDR and Deps"
+sudo apt -y install libsoapysdr-dev
+sudo apt -y install soapysdr-module-all
+sudo apt -y install soapysdr-module-xtrx 
+sudo apt -y install soapysapt-cache search bladerf-*dr-tools
+sudo apt -y install libsoapysdr-doc
+sudo apt -y install uhd-host
+sudo apt -y install uhd-soapysdr
+echo ""
 
-if [ SOAPYSDR == Y ]; then
-	echo "Installing SoapySDR and Deps"
-	apt -y install libsoapysdr-dev
-	apt -y install soapysdr-module-all
-	apt -y install soapysdr-module-xtrx 
-	apt -y install soapysdr-tools
-	apt -y install libsoapysdr-doc
-	apt -y install uhd-host
-	apt -y install uhd-soapysdr
+################################################################
+#
+# INSTALL SDRPLAY API for Linux
+#
+################################################################
+echo "Installing SDRPLAY API FOR Linux And SOAPYSDR"
+wget https://www.sdrplay.com/software/SDRplay_RSP_API-Linux-3.14.0.run
+sudo chmod 755 ./SDRplay_RSP_API-Linux-3.14.0.run
+sudo ./SDRplay_RSP_API-Linux-3.14.0.run
+echo ""
 
-	################################################################
-	#
-	# INSTALL SDRPLAY API for Linux
-	#
-	################################################################
-	echo "Installing SDRPLAY API FOR Linux And SOAPYSDR"
-	################################################################
-	# download the API from the SDRplay website
-	################################################################
-	wget https://www.sdrplay.com/software/SDRplay_RSP_API-Linux-3.14.0.run
-
-	################################################################
-	# change permission so the run file is executable
-	################################################################
-	chmod 755 ./SDRplay_RSP_API-Linux-3.14.0.run
-
-	################################################################
-	# execute the API installer (follow the prompts)
-	################################################################
-	./SDRplay_RSP_API-Linux-3.14.0.run
-
-	################################################################
-	#Get the sdrplay soapy module and build it.
-	################################################################
-	git clone https://github.com/pothosware/SoapySDRPlay3.git
-	cd SoapySDRPlay3
-	mkdir build
-	cd build
-	cmake ..
-	make
-	sudo make install
-fi
+################################################################
+# GIt the sdrplay soapy module and build it.
+################################################################
+echo " building and installing SoapySDRPlay3"
+git clone https://github.com/pothosware/SoapySDRPlay3.git
+cd SoapySDRPlay3
+mkdir build
+cd build
+cmake ..
+make
+sudo make install
+echo ""
 
 echo ""
 echo "IF any errors please report them so that they can be fixed"
